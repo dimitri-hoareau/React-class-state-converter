89cfb6d7550838975a20fa47d422f98eba56a9af
tonytiratayoclock <tony@oclock.io>
2020-07-15 10:01:39 +0200

    Amount Static done

--- src/components/Amount/index.js
+++ src/components/Amount/index.js
@@ -1,8 +1,13 @@
 import React from "react";
-import "./style.css";
+import "./style.scss";
 
 const Amount = () => {
-  return <div className="amount">amount</div>;
+  return (
+    <div className="amount">
+      <p className="amount-value">1.09</p>
+      <p className="amount-currency">United States Dollar</p>
+    </div>
+  );
 };
 
 export default Amount;
