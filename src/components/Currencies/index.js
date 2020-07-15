6ee9c14adfc31ce7a56f49e8839bff59cfcc691e
tonytiratayoclock <tony@oclock.io>
2020-07-15 09:57:58 +0200

    Currencies static done

--- src/components/Currencies/index.js
+++ src/components/Currencies/index.js
@@ -1,8 +1,31 @@
 import React from "react";
-import "./style.css";
+import "./style.scss";
 
 const Currencies = () => {
-  return <div className="currencies">currencies</div>;
+  return (
+    <div className="currencies">
+      <div className="currencies-title">Currencies</div>
+      <ul className="currencies-list">
+        <li className="currency">USD</li>
+        <li className="currency">USD</li>
+        <li className="currency">USD</li>
+        <li className="currency">USD</li>
+        <li className="currency">USD</li>
+        <li className="currency">USD</li>
+        <li className="currency">USD</li>
+        <li className="currency">USD</li>
+        <li className="currency">USD</li>
+        <li className="currency">USD</li>
+        <li className="currency">USD</li>
+        <li className="currency">USD</li>
+        <li className="currency">USD</li>
+        <li className="currency">USD</li>
+        <li className="currency">USD</li>
+        <li className="currency">USD</li>
+        <li className="currency">USD</li>
+      </ul>
+    </div>
+  );
 };
 
 export default Currencies;
