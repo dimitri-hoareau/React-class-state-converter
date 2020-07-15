89b7a3b3c0756918b93afc97b587d41b9b48409e
tonytiratayoclock <tony@oclock.io>
2020-07-15 09:16:53 +0200

    Basic components done

---
+++ src/components/Amount/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./style.css";
+
+const Amount = () => {
+  return <div className="amount">amount</div>;
+};
+
+export default Amount;
---
+++ src/components/Currencies/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./style.css";
+
+const Currencies = () => {
+  return <div className="currencies">currencies</div>;
+};
+
+export default Currencies;
